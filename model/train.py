19a0d34c9c5283c659e02299a6a0412c89a0fdb5
Aseuc <busse1997@googlemail.com>
2023-11-18 02:00:20 +0100

    Changes

--- model/train.py
+++ model/train.py
@@ -11,40 +11,24 @@ from logging import warning
 from PIL import Image
 from tqdm.notebook import tqdm
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
 from tensorflow.keras.utils import load_img
 from tensorflow.keras.callbacks import Callback, EarlyStopping
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from keras.models import Sequential, Model, load_model
 from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D, Input
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
-warnings.filterwarnings('ignore')
 
-print("Test")
+class MyCallback(Callback):
+    def __init__(self, threshold):
+        super(MyCallback, self).__init__()
+        self.threshold = threshold
 
-BASE_DIR = 'data/img_align_celeba'
-image_paths = []
-image_filenames = os.listdir(BASE_DIR)
-
-for image in tqdm(image_filenames):
-  image_path = os.path.join(BASE_DIR, image)
-  image_paths.append(image_path)
-
-len(image_paths)
-image_paths
-
-
-
-list_attr_celebaCSV = 'data/list_attr_celeba.csv'
-df = pd.read_csv(list_attr_celebaCSV)
-df_Male = df["Male"]
-df_Male = df_Male.replace(-1, 0)
-df_gender = df_Male.values
-df = pd.DataFrame({'Images':image_paths, 'Gender': df_gender})
-first_row = df.iloc[0]
-image_df = df.drop("Gender", axis=1)
-
-
-
+    def on_epoch_end(self, epoch, logs=None): 
+        val_acc = logs.get('val_accuracy')
+        if val_acc is not None and val_acc >= self.threshold:
+            self.model.stop_training = True
+            # Save the model
+            self.model.save(f'{n}.h5')
 
 def find_file(filename, directory):
     for root, dirs, files in os.walk(directory):
@@ -56,23 +40,6 @@ def find_file(filename, directory):
 class StopExecution(Exception):
     def _render_traceback_(self):
         pass
-
-n = 2000
-
-directory = 'C:/Users/busse/CICDPipeline/BA-CICD-Pipeline/model'
-filename = f'trained_{n}_model.h5'  
-file_path = find_file(filename, directory)
-
-print(n)
-df_sample = df.groupby('Gender', group_keys=False).apply(lambda x: x.sample(n=n, random_state=42))
-sns.displot(df_sample["Gender"])
-df_sample
-
-df_sample = df_sample.sample(frac=1, random_state=42).reset_index(drop=True)
-df_sample.to_excel(f'Gender_{n}.xlsx', index=False)
-
-
-
 
 def extract_image_features(images):
     features = list()
@@ -88,36 +55,39 @@ def extract_image_features(images):
     features = features.reshape(len(features), 178, 218, 1)
     return features
 
+
+BASE_DIR = 'data/img_align_celeba'
+image_paths = []
+image_filenames = os.listdir(BASE_DIR)
+
+for image in tqdm(image_filenames):
+  image_path = os.path.join(BASE_DIR, image)
+  image_paths.append(image_path)
+
+len(image_paths)
+image_paths
+list_attr_celebaCSV = 'data/list_attr_celeba.csv'
+df = pd.read_csv(list_attr_celebaCSV)
+df_Male = df["Male"]
+df_Male = df_Male.replace(-1, 0)
+df_gender = df_Male.values
+df = pd.DataFrame({'Images':image_paths, 'Gender': df_gender})
+n = 2000
+directory = 'C:/Users/busse/CICDPipeline/BA-CICD-Pipeline/model'
+filename = f'trained_{n}_model.h5'  
+file_path = find_file(filename, directory)
+print(n)
+df_sample = df.groupby('Gender', group_keys=False).apply(lambda x: x.sample(n=n, random_state=42))
+sns.displot(df_sample["Gender"])
+df_sample
+df_sample = df_sample.sample(frac=1, random_state=42).reset_index(drop=True)
+df_sample.to_excel(f'Gender_{n}.xlsx', index=False)
 X = extract_image_features(df_sample['Images'])
-
 X = X / 255.0
-
 X.shape
-
 y_gender = np.array(df_sample['Gender'])
-
-
-
-
-class MyCallback(Callback):
-    def __init__(self, threshold):
-        super(MyCallback, self).__init__()
-        self.threshold = threshold
-
-    def on_epoch_end(self, epoch, logs=None): 
-        val_acc = logs.get('val_accuracy')
-        if val_acc is not None and val_acc >= self.threshold:
-            self.model.stop_training = True
-            # Save the model
-            self.model.save(f'{n}.h5') 
-
-
-
-
 # Assuming X is your feature set and y_gender is your target variable
 X_train, X_test, y_train, y_test = train_test_split(X, y_gender, test_size=0.2, random_state=42)
-
-
 input_shape = (178, 218, 1)
 inputs = Input((input_shape))
 conv_1 = Conv2D(32, kernel_size=(4, 4), activation='relu')(inputs)
