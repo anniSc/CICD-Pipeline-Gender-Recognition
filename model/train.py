204793315330d34bf6702c96471820d92d318ce5
Aseuc <busse1997@googlemail.com>
2023-11-20 21:00:28 +0100

    Bug Fixing in test.py

--- model/train.py
+++ model/train.py
@@ -24,8 +24,8 @@ def extract_image_features(images):
     return features
 
 n = 100
-# with open("CICD-Pipeline-Gender-Recognition/test/n.txt", "w") as f:
-#     f.write(str(n))
+with open("test/n.txt", "w") as f:
+     f.write(str(n))
 
 directory = '/model'
 filename = f'trained_{n}_model.h5'  
