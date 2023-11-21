661b97b53805a164c040c3e513ac2618b52142ed
Aseuc <busse1997@googlemail.com>
2023-11-21 16:16:39 +0100

    Refactor file path in dataprep.py

--- data/dataprep.py
+++ data/dataprep.py
@@ -19,22 +19,22 @@ df_sample = df.groupby('Gender', group_keys=False).apply(lambda x: x.sample(n=n,
 df_sample = df_sample.sample(frac=1, random_state=42).reset_index(drop=True)
 df_sample.to_excel(f'model/excel_sheets/Gender_{n}.xlsx', index=False)
 file_names = df_sample['Images'].tolist()
-
+print(file_names)
 # Liste der Dateipfade erstellen
-df_sample['Images'] = df_sample['Images'].str.replace('data/img_align_celeba', 'data/selected_images')
-df_sample.to_excel(f'model/excel_sheets/Gender_{n}.xlsx', index=False)
+# df_sample['Images'] = df_sample['Images'].str.replace('data/img_align_celeba', 'data/selected_images')
+# df_sample.to_excel(f'model/excel_sheets/Gender_{n}.xlsx', index=False)
 
-source_folder = "data/img_align_celeba"
-destination_folder = 'data/selected_images'
+# source_folder = "data/img_align_celeba"
+# destination_folder = 'data/selected_images'
 
 
-# Erstelle eine Liste der Dateipfade der Bilder in "selected_images" basierend auf den Dateinamen in "df_sample"
-selected_image_paths = [os.path.join(source_folder, file) for file in file_names]
+# # Erstelle eine Liste der Dateipfade der Bilder in "selected_images" basierend auf den Dateinamen in "df_sample"
+# selected_image_paths = [os.path.join(source_folder, file) for file in file_names]
 
-# Überprüfe, ob der Dateipfad existiert und kopiere die Datei in den Zielordner
-for image_path in selected_image_paths:
-     if os.path.exists(image_path):
-          print(image_path)
-          shutil.copy(image_path, destination_folder)
+# # Überprüfe, ob der Dateipfad existiert und kopiere die Datei in den Zielordner
+# for image_path in selected_image_paths:
+#      if os.path.exists(image_path):
+#           print(image_path)
+#           shutil.copy(image_path, destination_folder)
 
 
