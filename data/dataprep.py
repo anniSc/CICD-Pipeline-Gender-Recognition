f69b2e0709aa7a296cb187241d8508570727089f
Aseuc <busse1997@googlemail.com>
2023-11-21 16:37:52 +0100

    Refactor data preparation script and update image copying logic

--- data/dataprep.py
+++ data/dataprep.py
@@ -3,15 +3,8 @@ import subprocess
 import pandas as pd
 import shutil
 
-
 # Speichern der Anzahl der zu trainierenden Bilder in einer Textdatei
 n = 100
-# with open("test/n.txt", "w") as f:
-#      f.write(str(n))
-
-# with open("test/test.py", "w") as f:
-#      f.write(str(n))
-
 
 # CSV lesen und für ML-Training vorbereiten
 df = pd.read_csv("data/Gender.csv")
@@ -20,22 +13,25 @@ df_sample = df_sample.sample(frac=1, random_state=42).reset_index(drop=True)
 df_sample.to_excel(f'model/excel_sheets/Gender_{n}.xlsx', index=False)
 file_names = df_sample['Images'].tolist()
 print(file_names)
+
 # Liste der Dateipfade erstellen
 df_sample['Images'] = df_sample['Images'].str.replace('data/img_align_celeba', 'data/selected_images')
-# df_sample.to_excel(f'model/excel_sheets/Gender_{n}.xlsx', index=False)
 
 # source_folder = "data/img_align_celeba"
 destination_folder = 'data/selected_images'
 
-# Erstelle eine Liste der Dateipfade der Bilder in "selected_images" basierend auf den Dateinamen in "df_sample"
-# selected_image_paths = [os.path.join(source_folder, file) for file in file_names]
-# for file in selected_image_paths:
-#      print(file)
+# Lösche die Bilder im "selected_images" Ordner, wenn er nicht leer ist
+if os.listdir(destination_folder):
+     for file_name in os.listdir(destination_folder):
+          file_path = os.path.join(destination_folder, file_name)
+          os.remove(file_path)
 
+          
 # Überprüfe, ob der Dateipfad existiert und kopiere die Datei in den Zielordner
 for image_path in file_names:
-     if os.path.exists(image_path):
-          print(image_path)
-          shutil.copy(image_path, destination_folder)
+      if os.path.exists(image_path):
+            print(image_path)
+            shutil.copy(image_path, destination_folder)
+
 
 
