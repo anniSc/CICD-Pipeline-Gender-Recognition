9bd1fa6e657f4991f6d58e061117248c59c8c4a6
Aseuc <busse1997@googlemail.com>
2023-11-22 10:15:18 +0100

    Update n value in dataprep.py

--- data/dataprep.py
+++ data/dataprep.py
@@ -4,7 +4,7 @@ import pandas as pd
 import shutil
 
 # Speichern der Anzahl der zu trainierenden Bilder in einer Textdatei
-n = 5000
+n = 1000
 
 with open("test/n.txt", "w") as f:
      f.write(str(n))
