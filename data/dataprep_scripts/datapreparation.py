5609575ea7df756be316c04990a016b3678b8326
Aseuc <busse1997@googlemail.com>
2024-02-08 14:22:39 +0100

    Update total_images variable to 1000

--- data/dataprep_scripts/datapreparation.py
+++ data/dataprep_scripts/datapreparation.py
@@ -456,7 +456,7 @@ class Main(DataPreparation, DataTest, DataBalancing, DataVisualization):
     """
     Die Hauptklasse, die die verschiedenen Funktionen zur Datenverarbeitung, Datenprüfung, Datenbalancierung und Datenvisualisierung enthält.
     """
-    total_images = 10
+    total_images = 1000
     balanced_gender_path = "data/balanced_source_csv/gender_balanced.csv"
     balanced_young_path = "data/balanced_source_csv/young_balanced.csv"
     young_column = "Young" 
