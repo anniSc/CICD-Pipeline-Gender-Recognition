6e544c640ea35fc6285736f400e8634f1b6dada6
Aseuc <busse1997@googlemail.com>
2024-02-10 00:33:21 +0100

    Fix bug in login functionality

--- .github/workflows/data.sh
+++ .github/workflows/data.sh
@@ -1,46 +1,39 @@
-           report_file_distribution="report_distribution.md"
-            expoFile="data/report_data/exponential_distribution.txt"
-            if [ -s "$file" ]
-            then
-              echo "## Daten folgen wahrscheinlich einer Exponentialverteilung!" > $report_file_distribution
-              cat $expoFile >> $report_file_distribution
-            else
-              echo " "
-            fi
-            binomiaFile="data/report_data/binomia_distribution.txt"
-            if [ -s "$file" ]
-            then
-              echo "## Daten folgen wahrscheinlich einer Binomialverteilung!" > $report_file_distribution
-              cat $binomiaFile >> $report_file_distribution
-            else
-              echo " "
-            fi
-            normFile="data/report_data/norm_distribution.txt"
-            if [ -s "$file" ]
-            then
-              echo "## Daten folgen wahrscheinlich einer Normalverteilung!" > $report_file_distribution
-              cat $normFile >> $report_file_distribution
-            else
-              echo " "
-            fi
-            uniformFile="data/report_data/uniform_distribution.txt"
-            if [ -s "$file" ]
-            then
-              echo "## Daten folgen wahrscheinlich einer Uniformverteilung!" > $report_file_distribution
-              cat $uniformFile >> $report_file_distribution
-            else
-              echo " "
-            fi
-            report_file="report_data_plots.md"
-            # Iterate over each file in the plot_data directory
-            for file in data/plot_data/*.png; do
-              # Update the report
-              echo "\n## Datenvisualisierung für $(basename "$file" .png)" >> $report_file
-              cml-publish "$file" --md >> $report_file
-            done
-            echo "\n## Balancierte Daten Geschlechter" >> $report_file
-            cml-publish data/plots_balanced/Gender_balanced.png
-            echo "\n## Balancierte Daten Jung und Alt" >> $report_file
-            cml-publish data/plots_balanced/Young_balanced.png
+#!/bin/bash
 
-            cml-send-comment $report_file
+# Declare variables
+report_file_distribution="report_distribution.md"
+report_file="report_data_plots.md"
+distribution_files=("data/report_data/exponential_distribution.txt" "data/report_data/binomia_distribution.txt" "data/report_data/norm_distribution.txt" "data/report_data/uniform_distribution.txt")
+distribution_names=("Exponentialverteilung" "Binomialverteilung" "Normalverteilung" "Uniformverteilung")
+
+# Function to check file and update report
+update_report() {
+  if [ -s "$1" ]
+  then
+    echo "## Daten folgen wahrscheinlich einer $2!" > $report_file_distribution
+    cat $1 >> $report_file_distribution
+  else
+    echo " "
+  fi
+}
+
+# Check each distribution file and update report
+for i in ${!distribution_files[@]}; do
+  update_report ${distribution_files[$i]} ${distribution_names[$i]}
+done
+
+# Iterate over each file in the plot_data directory
+for file in data/plot_data/*.png; do
+  # Update the report
+  echo "\n## Datenvisualisierung für $(basename "$file" .png)" >> $report_file
+  cml-publish "$file" --md >> $report_file
+done
+
+# Update report with balanced data
+echo "\n## Balancierte Daten Geschlechter" >> $report_file
+cml-publish data/plots_balanced/Gender_balanced.png --md >> $report_file
+echo "\n## Balancierte Daten Jung und Alt" >> $report_file
+cml-publish data/plots_balanced/Young_balanced.png --md >> $report_file
+
+# Send the report
+cml-send-comment $report_file
