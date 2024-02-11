cac1a76e7178ec0a31271040aaf42e93c1b5d093
Aseuc <busse1997@googlemail.com>
2024-02-12 00:56:33 +0100

    Delete unused files and update total_images count

--- .github/workflows/data.sh
+++ .github/workflows/data.sh
@@ -28,27 +28,18 @@ create_single_report() {
 }
 
 
-
-
-# Check each distribution file and update report
 for i in ${!distribution_files[@]}; do
   update_report ${distribution_files[$i]} ${distribution_names[$i]}
 done
 
-# Iterate over each file in the plot_data directory
 for file in data/plot_data/*.png; do
-  # Update the report
   echo "\n## Datenvisualisierung für $(basename "$file" .png)" >> $report_file
   cml-publish "$file" --md >> $report_file
 done
 
-# Update report with balanced data
 echo "\n## Balancierte Daten Geschlechter" >> $report_file
 cml-publish data/plots_balanced/Gender_balanced.png --md >> $report_file
 echo "\n## Balancierte Daten Jung und Alt" >> $report_file
 cml-publish data/plots_balanced/Young_balanced.png --md >> $report_file
 
-# Send the report
 cml-send-comment $report_file
-
-create_single_report "report_ml.md" "CPU/Speicherauslastung" "data/cpu_memory_usage_on_dataprep.png"
---
+++ reports/report_rotation.md
@@ -0,0 +1,2 @@
+## Modellmetriken mit verdrehte Bilder
+![](https://asset.cml.dev/858e274930bbd4f2334f8b373bb1eeb6af5dc2c6?cml=png)
