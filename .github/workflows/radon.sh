82ed82207a8fea19e5bb95ce30e9f1b4a358d373
Aseuc <busse1997@googlemail.com>
2024-02-12 06:06:05 +0100

    Fix array iteration in radon.sh script

--- .github/workflows/radon.sh
+++ .github/workflows/radon.sh
@@ -1,21 +1,21 @@
 python_files=("model/model_script/model_train.py" "deploy/deploy.py" "data/dataprep_scripts/datapreparation.py", "test/model_test_scripts/model_test.py")
 
 # Run radon cc
-for python_file in "${python_files[@]}"; do
+for python_file in "${python_files}"; do
     datetime=$(date +%Y%m%d_%H%M%S)
     output_file_cc="radon_tests/output_cc_max.txt"
     radon cc ${python_file} > ${output_file_cc}
 done
 
 # Run radon raw
-for python_file in "${python_files[@]}"; do
+for python_file in "${python_files}"; do
     datetime=$(date +%Y%m%d_%H%M%S)
     output_file_raw="radon_tests/output_raw_max.txt"
     radon raw ${python_file} > ${output_file_raw}
 done
 
 # Run radon mi
-for python_file in "${python_files[@]}"; do
+for python_file in "${python_files}"; do
     datetime=$(date +%Y%m%d_%H%M%S)
     output_file_mi="radon_tests/output_mi_max.txt"
     radon mi ${python_file} > ${output_file_mi}
