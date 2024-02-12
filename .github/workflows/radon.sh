b71bf67ea4e096f721084b297fddae4ae824b8c7
Aseuc <busse1997@googlemail.com>
2024-02-12 05:22:23 +0100

    Refactor radon.sh script to separate functions for each radon metric calculation

--- .github/workflows/radon.sh
+++ .github/workflows/radon.sh
@@ -1,25 +1,27 @@
-#!/bin/bash
 
-run_radon() {
+run_radon_cc() {
     local python_file=$1
-
-    datetime=$(date +%Y%m%d_%H%M%S)
     output_file_cc="radon_tests/output_cc_max.txt"
-    output_file_raw="radon_tests/output_raw_max.txt"
-    output_file_mi="radon_tests/output_mi_max.txt"
-
     radon cc ${python_file} > ${output_file_cc}
-    radon raw ${python_file} > ${output_file_raw}
-    radon mi ${python_file} > ${output_file_mi}
 }
 
+run_radon_raw() {
+    local python_file=$1
+    output_file_raw="radon_tests/output_raw_max.txt"
+    radon raw ${python_file} > ${output_file_raw}
+}
 
-
-
+run_radon_mi() {
+    local python_file=$1
+    output_file_mi="radon_tests/output_mi_max.txt"
+    radon mi ${python_file} > ${output_file_mi}
+}
 
 # List of python files
 python_files=("model/model_script/model_train.py" "deploy/deploy.py" "data/dataprep_scripts/datapreparation.py", "test/model_test_scripts/model_test.py")
 
 for python_file in "${python_files[@]}"; do
-    run_radon $python_file
+    run_radon_cc $python_file
+    run_radon_raw $python_file
+    run_radon_mi $python_file
 done
