b087a7a9319ae158444ae74a8d3ea5259d964751
Aseuc <busse1997@googlemail.com>
2024-02-12 05:07:33 +0100

    Fix radon.sh script to correctly redirect output to files

--- .github/workflows/radon.sh
+++ .github/workflows/radon.sh
@@ -8,12 +8,12 @@ run_radon() {
     output_file_raw="output_${datetime}_${python_file}.txt"
     output_file_mi="output_${datetime}_${python_file}.txt"
     radon cc ${python_file} > ${output_file_cc}
-    radon raw ${python_file} >> ${output_file_raw}
-    radon mi ${python_file} >> ${output_file_mi}
+    radon raw ${python_file} > ${output_file_raw}
+    radon mi ${python_file} > ${output_file_mi}
 }
 
 # List of python files
-python_files=("model/model_script/model_train.py" "deploy/deploy.py" "data/dataprep_scripts/datapreparation.py", "test/model_test_scripts/model_test.py", "test/data_test_scripts/data_test.py")
+python_files=("model/model_script/model_train.py" "deploy/deploy.py" "data/dataprep_scripts/datapreparation.py", "test/model_test_scripts/model_test.py")
 
 for python_file in "${python_files[@]}"; do
     run_radon $python_file
