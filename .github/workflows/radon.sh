b7b209591f62f9eb085caf1b720bc01b57e3f86a
Aseuc <busse1997@googlemail.com>
2024-02-12 16:43:46 +0100

    Update code quality workflow and dependencies

---
+++ .github/workflows/radon.sh
@@ -0,0 +1,3 @@
+radon cc "model/model_script/model_train.py" "deploy/deploy.py" "data/dataprep_scripts/datapreparation.py" "test/model_test_scripts/model_test.py"> radon_tests/output_radon_cc_max.txt
+radon mi "model/model_script/model_train.py" "deploy/deploy.py" "data/dataprep_scripts/datapreparation.py" "test/model_test_scripts/model_test.py"> radon_tests/output_radon_mi_max.txt
+radon raw "model/model_script/model_train.py" "deploy/deploy.py" "data/dataprep_scripts/datapreparation.py" "test/model_test_scripts/model_test.py"> radon_tests/output_radon_raw_max.txt
