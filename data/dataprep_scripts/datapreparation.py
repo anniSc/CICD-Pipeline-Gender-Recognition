1a22208189a2a218fd7c65a0151acf84851fe645
Aseuc <busse1997@googlemail.com>
2024-02-15 23:15:09 +0100

    Update model training script and data preparation script

--- data/dataprep_scripts/datapreparation.py
+++ data/dataprep_scripts/datapreparation.py
@@ -764,7 +764,7 @@ class Main(DataPreparation, DataTest, DataBalancing, DataVisualization):
             Rückgabewert:
             - None
             """
-            self.total_images = 10
+            self.total_images = 10000
             self.balanced_gender_path = "data/balanced_source_csv/gender_balanced.csv"
             self.balanced_young_path = "data/balanced_source_csv/young_balanced.csv"
             self.young_column = "Young"
